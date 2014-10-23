d38fdb1e1a4e59a9b1edf6780e84cb5a4633d2b2
Ivan Lopez <ivan@10up.com>
2014-10-23 10:41:04 -0400

    add PHPDock Blocks

--- Database/Validator.php
+++ Database/Validator.php
@@ -4,22 +4,41 @@ namespace TenUp\Exodus\Database;
 
 use TenUp\Exodus\Schema\Base_Schema;
 
+/**
+ * Class Validator
+ * @package TenUp\Exodus\Database
+ */
 class Validator {
 
+	/**
+	 * @var array indexes of content items to check
+	 */
 	public $check = array();
 
+	/**
+	 * Build out the random list of post to check.
+	 *
+	 * @param int $percent 1-100
+	 * @param int $total   number of content items imported
+	 */
 	public function setup( $percent, $total ) {
 		$item_count = round( $total * ( $percent / 100 ) );
-		if( $total > 1){
-			$items  = range( 0, ( $total - 1 ) );
+		if ( $total > 1 ) {
+			$items       = range( 0, ( $total - 1 ) );
 			$this->check = array_rand( $items, $item_count );
 		} else {
 			$this->check = array( 0 );
 		}
-
-
 	}
 
+	/**
+	 * Check to see if the imported content matches whats in the database.
+	 *
+	 * @param stdclass $content import content object
+	 * @param int      $id      imported post id
+	 *
+	 * @return bool
+	 */
 	public function compare( $content, $id ) {
 		$post = get_post( $id );
 
@@ -29,9 +48,9 @@ class Validator {
 					return $id;
 				}
 			} else {
-				if( 'meta_data' === $key && ! $this->check_post_meta( $value, $id ) ) {
+				if ( 'meta_data' === $key && ! $this->check_post_meta( $value, $id ) ) {
 					return $id;
-				} elseif( 'taxonomy' === $key && ! $this->check_post_terms( $value, $id ) ){
+				} elseif ( 'taxonomy' === $key && ! $this->check_post_terms( $value, $id ) ) {
 					return $id;
 				}
 			}
@@ -40,22 +59,37 @@ class Validator {
 		return true;
 	}
 
+	/**
+	 * Check to see if the id passes is in the $check array.
+	 *
+	 * @param int $id
+	 *
+	 * @return bool
+	 */
 	public function should_compare( $id ) {
 		return in_array( $id, $this->check ) ? true : false;
 	}
 
-	protected function check_post_meta( $meta_data, $id ){
+	/**
+	 * Check to see if the imported meta data matches whats in the database.
+	 *
+	 * @param array $meta_data
+	 * @param int $id
+	 *
+	 * @return bool
+	 */
+	protected function check_post_meta( $meta_data, $id ) {
 		$meta = get_post_meta( $id );
-		if( $meta ){
+		if ( $meta ) {
 			$errors = 0;
-			foreach( $meta_data as $meta_key => $meta_value ){
-				if( isset($meta[ $meta_key ]) && $meta[ $meta_key ][0] === $meta_value ){
+			foreach ( $meta_data as $meta_key => $meta_value ) {
+				if ( isset( $meta[ $meta_key ] ) && $meta[ $meta_key ][0] === $meta_value ) {
 					continue;
 				} else {
-					$errors++;
+					$errors ++;
 				}
 			}
-			if( $errors > 0){
+			if ( $errors > 0 ) {
 				return false;
 			}
 		}
@@ -63,13 +97,21 @@ class Validator {
 		return true;
 	}
 
-	protected function check_post_terms( $taxonomies, $id ){
+	/**
+	 * Check to see if imported taxonomy matches whats in the database.
+	 *
+	 * @param array $taxonomies
+	 * @param int $id
+	 *
+	 * @return bool
+	 */
+	protected function check_post_terms( $taxonomies, $id ) {
 		$errors = 0;
 		foreach ( $taxonomies as $tax_key => $terms ) {
-			if( ! has_term( $terms, $tax_key, $id ) ){
-				$errors++;
+			if ( ! has_term( $terms, $tax_key, $id ) ) {
+				$errors ++;
 			}
-			if( $errors > 0){
+			if ( $errors > 0 ) {
 				return false;
 			}
 		}
@@ -77,6 +119,15 @@ class Validator {
 		return true;
 	}
 
+	/**
+	 * Empty out the src attribute in order to take into consideration
+	 * that the url of the images imported will not match.
+	 *
+	 * @param $content
+	 * @param $key
+	 *
+	 * @return mixed
+	 */
 	protected function strip_image_paths( $content, $key ) {
 		return ( 'post_content' === $key ) ? $content : preg_replace( "~src=[']([^']+)[']~e", '', $content );
 	}
